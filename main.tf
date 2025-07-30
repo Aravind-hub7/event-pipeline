092361989883f37fcf1a23e0cb24292b9e0d1b2b
Aravind-hub7 <karavindgoud143@gmail.com>
2025-07-30 19:55:57 +0530

    Added S3 remote backend to store Terraform state

--- main.tf
+++ main.tf
@@ -1,3 +1,62 @@
+terraform {
+  backend "s3" {
+    bucket = "terraform-backend-aravind-2025"
+    key    = "event-pipeline/terraform.tfstate"
+    region = "us-east-1"
+  }
+}
+
+provider "aws" {
+  region = "us-east-1"
+}
+
+# S3 bucket
+resource "aws_s3_bucket" "data_bucket" {
+  bucket = "event-pipeline-aravind-2025"
+}
+
+# IAM role
+resource "aws_iam_role" "lambda_exec" {
+  name = "lambda_exec_role_event_pipeline"
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [{
+      Action = "sts:AssumeRole",
+      Principal = { Service = "lambda.amazonaws.com" },
+      Effect = "Allow"
+    }]
+  })
+}
+
+# IAM role policy
+resource "aws_iam_role_policy" "lambda_policy" {
+  name = "lambda_policy"
+  role = aws_iam_role.lambda_exec.id
+  policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Effect = "Allow",
+        Action = [
+          "s3:PutObject",
+          "logs:*"
+        ],
+        Resource = "*"
+      }
+    ]
+  })
+}
+
+# Lambda function
+resource "aws_lambda_function" "event_processor" {
+  function_name    = "event_processor"
+  role             = aws_iam_role.lambda_exec.arn
+  handler          = "lambda_function.lambda_handler"
+  runtime          = "python3.9"
+  filename         = "lambda_function_payload.zip"
+  source_code_hash = filebase64sha256("lambda_function_payload.zip")
+}
+
 provider "aws" {
   region = "us-east-1"
 }
